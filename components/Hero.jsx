b655c7a2b1184ca7e18b89931c14531ddfaeebd7
Wil Hesk <wilhesk@gmail.com>
2023-06-02 23:55:56 +0100

    heroedit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = ({heading, message, button, heading2, message2 }) => {
     <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img'>
         {/* Overlay */}
         <div className='absolute top-0 left-0 right-0 bottom-0 bg-dkgrey/70 z-[2]' />
-        <div className='p-5 text-white z-[2] mt-[4rem] mr-[5%] ml-[5%]'>
+        <div className='p-5 text-white z-[2] mt-[4rem] mr-[20%] ml-[20%]'>
             <div>
               <h2 className='text-5xl font-normal '>{heading}</h2>
               <p className='py-5 text-xl '>{message}</p>
