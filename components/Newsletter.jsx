48ee1fc4ad5f8d7e4128e71cb0086a2a93157563
Wil Hesk <wilhesk@gmail.com>
2023-07-19 21:28:38 +0100

    styling to Newsletter component for screen and mob

--- components/Newsletter.jsx
+++ components/Newsletter.jsx
@@ -19,23 +19,23 @@ export default () => {
   };
 
   return (
-    <div className="flex place-content-center flex-col py-28 items-center align-middle bg-peach text-white mt-1">
+    <div className="flex place-content-center flex-col items-center align-middle bg-peach text-white mt-1 h-screen">
       <h2 className="text-3xl font-bold mt-8 mb-4 md: w-1/2 text-left ">
         Your Community Classes
       </h2>
       <p className="mt-2 mb-4 text-dkblue font-light w-1/2 text-left">
         Sign up for information on community classes by entering your email below. I don't send lots of emails, but I can keep you up to date with class times and locations.
       </p>
-      <div className="flex sm:w-4/5 justify-center mt-4 mb-11 flex-col lg:flex-row">
+      <div className="flex sm:w-4/5 justify-center mt-4 flex-col lg:flex-row">
         <input
-          className="appearance-none mb-2 lg:mb-0 w-full lg:w-2/3 border border-gray-500 rounded py-2 px-4 text-gray-700 text-dkblue leading-tight focus:outline-none focus:border-gray-600"
+          className="appearance-none mb-2 lg:mb-0 w-full lg:w-2/3 rounded py-2 px-4 text-dkblue text-center leading-tight focus:outline-none focus:border border-dkblue"
           type="text"
           placeholder="Enter Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
         <button
-          className={`lg:ml-2 lg:w-1/5 shadow focus:shadow-outline focus:outline-none text-dkblue bg-white font-light py-2 px-5 rounded flex hover:text-white hover:bg-dkblue ${
+          className={`lg:ml-2 lg:w-1/5 shadow text-dkblue bg-white font-light py-2 rounded flex justify-center hover:text-white hover:bg-dkblue ${
             state === "LOADING" ? "button-gradient-loading" : ""
           }`}
           type="button"
@@ -45,12 +45,14 @@ export default () => {
           Submit
         </button>
       </div>
-      {state === "ERROR" && (
-        <p className="w-1/2 mt-2 text-red-600">{errorMessage}</p>
-      )}
-      {state === "SUCCESS" && (
-        <p className="w-1/2 mt-2 text-green-600">Success!</p>
-      )}
+        <div className="w-1/2 text-left mt-4">
+        {state === "ERROR" && (
+          <p className="text-white tracking-wide text-xl">{errorMessage}</p>
+        )}
+        {state === "SUCCESS" && (
+          <p className="text-white tracking-wide text-xl">Email submitted, thanks!</p>
+        )}
+        </div>
     </div>
   );
 };
