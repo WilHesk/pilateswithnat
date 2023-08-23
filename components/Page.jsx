19a3c4ed1b273f9712d89c0f4a56acffb0bdc6ad
Wil Hesk <wilhesk@gmail.com>
2023-08-23 09:56:50 +0100

    img switch mob styling

--- components/Page.jsx
+++ components/Page.jsx
@@ -3,10 +3,10 @@ import Link from 'next/link';
 
 const Page = ({heading, heading2, message2, para }) => {
   return (
-    <div className='flex items-center justify-center h-screen mt-0 bg-fixed bg-center bg-cover custom-img'>
+    <div className='flex items-center justify-center h-screen mt-0 bg-fixed bg-center bg-cover custom-img-mob md:custom-img'>
         {/* Overlay */}
         <div className='absolute top-0 left-0 right-0 bottom-0 bg-dkgrey/40 z-[2]' />
-        <div className='p-5 text-white z-[2] mt-[4rem] mr-[20%] ml-[30%]'>
+        <div className='p-5 text-white z-[2] mt-[4rem] mx-[6%] md:mr-[20%] md:ml-[30%]'>
             <div>
               <h2 className='text-5xl font-normal main-title'>{heading}</h2>
               <h2 className='pt-1 text-2xl font-thin text-peach'>{heading2}</h2>
--- components/Testi.jsx
+++ components/Testi.jsx
@@ -3,10 +3,10 @@ import Link from 'next/link';
 
 const Testi = ({heading1, heading2, message1, message2, message3 }) => {
   return (
-    <div className='flex items-center justify-center h-screen mt-0 bg-fixed bg-center bg-cover custom-img'>
+    <div className='flex items-center justify-center h-screen mt-0 bg-fixed bg-center bg-cover bg-cover custom-img-mob md:custom-img'>
         {/* Overlay */}
         <div className='absolute top-0 left-0 right-0 bottom-0 bg-dkgrey/70 z-[2]' />
-        <div className='p-5 text-white z-[2] mt-[4rem] mx-[4%] sm:mr-[15%] sm:ml-[25%] '>
+        <div className='p-5 text-white z-[2] mt-[4rem] mx-[6%] sm:mr-[15%] sm:ml-[25%] '>
             <div>
               <h2 className='text-5xl main-title'>{heading1}</h2>
               <h2 className='pt-1 pb-3 text-2xl font-thin text-peach'>{heading2}</h2>
