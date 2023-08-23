34e483010caa012bddae3f1cc92897d387b51d8e
Wil Hesk <wilhesk@gmail.com>
2023-08-23 09:26:31 +0100

    styling etxt

--- components/Page.jsx
+++ components/Page.jsx
@@ -8,8 +8,8 @@ const Page = ({heading, heading2, message2, para }) => {
         <div className='absolute top-0 left-0 right-0 bottom-0 bg-dkgrey/40 z-[2]' />
         <div className='p-5 text-white z-[2] mt-[4rem] mr-[20%] ml-[30%]'>
             <div>
-              <h2 className='text-5xl font-normal'>{heading}</h2>
-              <h2 className='pt-1 text-2xl font-thin'>{heading2}</h2>
+              <h2 className='text-5xl font-normal main-title'>{heading}</h2>
+              <h2 className='pt-1 text-2xl font-thin text-peach'>{heading2}</h2>
               <p className='py-2 text-xl font-light'>{message2}</p>
               <p className='text-med  font-thin'>{para}</p>
               
